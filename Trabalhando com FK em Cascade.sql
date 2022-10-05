aa7c3a9014aac089dc21cc679bc3e88241c2bee1
Aislan Honorato <aislanmano@gmail.com>
2022-10-05 15:55:22 -0400

    Update Trabalhando com FK em Cascade.sql

--- Trabalhando com FK em Cascade.sql
+++ Trabalhando com FK em Cascade.sql
@@ -42,7 +42,7 @@ DROP TABLE aluno_curso;
     curso_id INTEGER,
     PRIMARY KEY (aluno_id, curso_id),
 
-    FOREIGN KEY (aluno_id) REFERENCES aluno (id)  ON DELETE CASCADE,
+    FOREIGN KEY (aluno_id) REFERENCES aluno (id)  ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY (curso_id) REFERENCES curso (id)
 );
 
